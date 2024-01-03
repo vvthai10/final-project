689da3f233b30552b8054926e7d8a9f4153f3d4b
nlduy0310 <nlduy0310@gmail.com>
2024-01-03 19:03:08 +0700

    add ghost to map

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,187 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostController : MonoBehaviour
+{
+    [Header("Attributes for chase and jumpscare")]
+    public Transform player;
+    public float jumpScareOffset = 2f;
+    //public PlayerCam cam;
+    public Transform jumpscareLookAtPoint;
+    public Light jumpscareLight;
+
+    private Animator animator;
+
+
+    private NavMeshAgent navMeshAgent;
+    private OffMeshLinkData offMeshLinkData;
+    private bool correctedRotationOnLink = false;
+
+
+    // animation state hashes
+    private int zombieBitingHash = Animator.StringToHash("Zombie Biting");
+    private int runningCrawlHash = Animator.StringToHash("Running Crawl");
+    private int zombieAttackHash = Animator.StringToHash("Zombie Attack");
+    private int rightWallLeapHash = Animator.StringToHash("Right Wall Leap");
+    private int leftWallLeapHash = Animator.StringToHash("Left Wall Leap");
+    private int injuredIdleHash = Animator.StringToHash("Injured Idle");
+
+    public enum WallLeapHorizontalDirection
+    {
+        Left,
+        Right
+    }
+
+    private bool chasing = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponentInParent<NavMeshAgent>();
+        StartChasing();
+        //animator.Play(zombieBitingHash);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    //public void RotateTowardsCamera()
+    //{
+    //    Vector3 orgDirection = (cam.transform.position - transform.position).normalized;
+    //    orgDirection.z = 0;
+    //    Quaternion lookRotation = Quaternion.LookRotation(orgDirection);
+    //    cam.transform.rotation = lookRotation;
+    //}
+
+    public void StartChaseSequence()
+    {
+        //this.StartBitingAnimation();
+        StartCoroutine(StartChasingAfter(3));
+    }
+
+    public void StartBitingAnimation()
+    {
+        animator.Play(zombieBitingHash);
+    }
+
+    public void StartChasingAnimation()
+    {
+        animator.Play(runningCrawlHash);
+    }
+
+    public void StartAttackAnimation()
+    {
+        animator.Play(zombieAttackHash);
+    }
+
+    public void StartWallLeapAnimation(WallLeapHorizontalDirection direction)
+    {
+        
+    }
+
+    public void StartChasing()
+    {
+        chasing = true;
+        navMeshAgent.enabled = true;
+        StartChasingAnimation();
+    }
+
+    public void StopChasing()
+    {
+        chasing = false;
+        navMeshAgent.enabled = false;
+    }
+
+    //public void StartJumpscare()
+    //{
+    //    player.GetComponent<PlayerMovement>().Freeze();
+    //    cam.Freeze();
+    //    this.RotateTowardsCamera();
+    //    this.StartAttackAnimation();
+    //    cam.StartRotatingTowards(this.jumpscareLookAtPoint);
+    //    jumpscareLight.enabled = true;
+    //}
+
+    IEnumerator StartChasingAfter(float seconds = 3)
+    {
+        yield return new WaitForSeconds(seconds);
+        StartChasing();
+    }
+
+    public void HandleNavmeshLinkTraversal()
+    {
+
+        offMeshLinkData = navMeshAgent.currentOffMeshLinkData;
+        Vector3 endpos = offMeshLinkData.endPos + Vector3.up * navMeshAgent.baseOffset;
+        navMeshAgent.transform.position = Vector3.MoveTowards(navMeshAgent.transform.position, endpos, navMeshAgent.speed * Time.deltaTime);
+
+
+        if (!correctedRotationOnLink)
+        {
+            Vector3 direction = offMeshLinkData.endPos - offMeshLinkData.startPos;
+            Debug.Log($"direction: {direction}");
+            // align with path's rotation towards
+            transform.rotation = Quaternion.LookRotation(direction);
+            
+            // flip character on ceiling links
+            if (IsOnCeilingNavMeshLink())
+            {
+                Debug.Log("OnCeilingNavMeshLink");
+                transform.Rotate(direction, 180, Space.World);
+            }
+
+            correctedRotationOnLink = true;
+        }
+        
+        if (navMeshAgent.transform.position == endpos)
+        {
+            Debug.Log("ending navlink");
+            correctedRotationOnLink = false;
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            navMeshAgent.CompleteOffMeshLink();
+            StartChasingAnimation();
+        }
+    }
+
+    private bool IsOnCeilingNavMeshLink()
+    {
+        try
+        {
+            return navMeshAgent.navMeshOwner.ToString().IndexOf("WallCeilingNavMeshLink", StringComparison.OrdinalIgnoreCase) > -1;
+        } catch
+        {
+            return false;
+        }
+    }
+
+    private void Update()
+    {
+        //if (chasing && Vector3.Distance(transform.position, player.position) <= jumpScareOffset)
+        //{
+        //    this.StopChasing();
+        //    this.StartJumpscare();
+        //}
+
+        if (chasing)
+        {
+            navMeshAgent.SetDestination(player.position);
+        }
+
+        if (navMeshAgent.isOnOffMeshLink)
+        {
+            HandleNavmeshLinkTraversal();
+        }
+    }
+}
