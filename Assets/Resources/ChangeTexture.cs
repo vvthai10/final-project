ce90c11059ef10f84e65747af263d59d9bb4e2a9
thaivv <thaivv@smartloyalty.vn>
2024-01-21 17:41:31 +0700

    update picture

---
+++ Assets/Resources/ChangeTexture.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ChangeTexture : MonoBehaviour
+{
+    public Texture newTexture; // Texture mới mà bạn muốn sử dụng
+    public Renderer picture;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Kiểm tra xem đối tượng đi vào trigger có phải là người chơi hay không
+        if (other.CompareTag("Player"))
+        {
+            // Gọi hàm để thay đổi texture khi đi vào trigger
+            ChangeObjectTexture(newTexture);
+        }
+    }
+
+    // Hàm để thay đổi texture của Material
+    void ChangeObjectTexture(Texture newTexture)
+    {
+        if (picture != null && newTexture != null)
+        {
+            picture.material.mainTexture = newTexture;
+        }
+        else
+        {
+            Debug.LogError("Renderer or newTexture is null.");
+        }
+    }
+}
