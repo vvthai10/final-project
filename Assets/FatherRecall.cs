e2b55edfb7319a02a04da0ea3da74db9a0c72dce
thaivv <thaivv@smartloyalty.vn>
2024-01-26 15:16:27 +0700

    update mother

---
+++ Assets/FatherRecall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FatherRecall : MonoBehaviour
+{
+    public Text uiText;  // Kéo và thả UI Text vào trường này trong Inspector
+    public float typingSpeed = 0.1f;  // Tốc độ hiển thị chữ
+
+    private bool isTyping = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isTyping)
+        {
+            isTyping = true;
+            StartCoroutine(TypeText());
+        }
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in "Chữ muốn hiển thị")
+        {
+            uiText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
